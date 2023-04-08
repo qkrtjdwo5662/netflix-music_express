a2bfc3bb77d614ac1ca37ec3432d1e7cbf88d0b4
qkrtjdwo5662 <qkrtjdwo5662@naver.com>
2023-04-08 13:26:30 +0900

    .

--- controllers/mbController.js
+++ controllers/mbController.js
@@ -1,12 +1,22 @@
 const connection = require('./dbConnect');
 
-const mbDB = {
-  getAll: (cb) => {
-    connection.query('select * from mainBanner;', (err, data) => {
-      if (err) throw err;
-      cb(data);
-    });
-  },
-};
+const getAll = (req, res)=> {
+  connection.query(
+    'select * from mainBanner;',
+    (err, data) => {
+      if(err) throw err;
+      if(data.length === 0) res.status(500).send('메인배너 못 가져옴');
+      const mainBanner = data;
+      connection.query(
+        `select * from album`, 
+        (err, data) => {
+          if(err) throw err;
+          if(data.length === 0) res.status(500).send('메인앨범 못 가져옴');
+          res.render('main.ejs',{mainBanner});
+        }
+      )
+    }
+  )
+}
 
-module.exports = mbDB;
+module.exports = {getAll};
--- routes/main.js
+++ routes/main.js
@@ -1,22 +1,10 @@
 const express = require('express');
 
-const mbDB = require('../controllers/mbController');
+const {getAll} = require('../controllers/mbController');
 const albumDB = require('../controllers/albumController');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  mbDB.getAll((data) => {
-    const mainBanner = data;
-    albumDB.getAll((data) => {
-      const album = data;
-      console.log(album);
-      res.render('main.ejs', { mainBanner, album });
-    });
-  });
-  // albumDB.insertImg('./img/main_album/main_album2.png', (data) => {
-  //   console.log(data);
-  // });
-});
+router.get('/', getAll);
 
 module.exports = router;
